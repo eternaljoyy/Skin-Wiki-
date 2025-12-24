ff75b47fe28036c6838ea6af06e5947244f29aa2
Joydeep Gill <joyydeep.gill@gmail.com>
2025-12-23 21:43:10 -0500

    Add Login class with username accessors and database method stubs

--- Login.kt
+++ Login.kt
@@ -14,4 +14,14 @@ class Login {
     fun setUsername(String username) {
         this.username = username;
     }
+
+    //Verufy against the DB whether the login creds are correct
+    fun verifyLogin() {
+
+    }
+
+    //Connect to the database, if unable to connect, throw errors
+    fun connectUserDatabase() {
+
+    } 
 }
