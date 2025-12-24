8c238a4ddb009004b42d77ca51e80a138804d249
Joydeep Gill <joyydeep.gill@gmail.com>
2025-12-23 21:38:08 -0500

    Removed logic from incorrect class

---
+++ Login.kt
@@ -0,0 +1,17 @@
+class Login {
+    var username: String = ""
+
+    fun main() {
+
+    }
+
+    // return the username
+    fun getUsername() {
+        return this.username;
+    }
+
+    // Change the username
+    fun setUsername(String username) {
+        this.username = username;
+    }
+}
