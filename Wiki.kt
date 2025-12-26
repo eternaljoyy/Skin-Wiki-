50323508c68e59b21c7c2257dc2e8cca42434463
Joydeep Gill <joyydeep.gill@gmail.com>
2025-12-25 22:10:34 -0500

    Added some basic functions to Wiki.kt

---
+++ Wiki.kt
@@ -0,0 +1,27 @@
+class Wiki {
+
+    // Function to search ingreidents by name
+    fun search() {
+
+    }
+
+    // Load and fetch data from a backend (ie: database, list, etc)
+    fun loadData() {
+
+    }
+
+    // Function which flags the ingredient as irritating
+    fun isIrritant() {
+
+    }
+
+    // Function that categorizes an ingredient by type (ie: exfoliant,
+    fun ingredientType() {
+
+    }
+
+    // Display the data as a list, on a widget, etc.
+    fun displayData() {
+
+    }
+}
